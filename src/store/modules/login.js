b72a8d8dc34de1f769c02d6ad410ab54eafa0164
jaironf <jaironf.94@gmail.com>
2024-06-17 13:25:40 +0200

    Modules login

---
+++ src/store/modules/login.js
@@ -0,0 +1,17 @@
+const state = {
+    email: [],
+    password: []
+};
+
+
+const mutations = {
+    increment(state) {
+        state.email,
+        state.password
+    }
+};
+
+export default {
+    state,
+    mutations
+};
